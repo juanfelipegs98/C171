25eaf04bda47e4f0124fc2cf298b17a1a84917e4
Juan Granados <felipegranados346@gmail.com>
2023-08-24 20:21:43 -0500

    trabajo b4

---
+++ src/main/java/B4/Main.java
@@ -0,0 +1,17 @@
+package B4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            String val = "";
+            int num = 0;
+             Double numeros = null;
+
+        } catch (Exception E) {
+
+
+        }
+
+    }
+}
